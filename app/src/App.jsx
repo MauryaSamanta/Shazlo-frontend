b09577c5bd869f26a9e410fde21736de49a1e2f2
naby <nabyendutukai.das@gmail.com>
2025-07-09 11:13:05 +0530

    inital web created

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { HomePage, ContactPage } from "./HomePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+      <ContactPage />
+    </>
+  );
+}
+
+export default App;
