d9dd4c51ff0d26ab0ce197c0165f8c94e6fe5480
MauryaSamanta <mauryasamanta1508@gmail.com>
2025-07-17 23:48:53 +0530

    Added the updated apk link in button

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -5,6 +5,7 @@ const Button = () => {
   return (
     <StyledWrapper>
       <button type="button" className="button">
+        <a href="/app-release.apk" download style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}>
         <span className="fold" />
         <div className="points_wrapper">
           <i className="point" />
@@ -20,6 +21,7 @@ const Button = () => {
         </div>
         <span className="inner"><svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5">
             <polyline points="13.18 1.37 13.18 9.64 21.45 9.64 10.82 22.63 10.82 14.36 2.55 14.36 13.18 1.37" /></svg>Download App</span>
+            </a>
       </button>
     </StyledWrapper>
   );
